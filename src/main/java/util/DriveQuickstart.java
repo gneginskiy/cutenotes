f698a23831e95ac3d570d5ba82818471002b98c2
bulavin.ilya <bulavin.ilya@otr.ru>
2020-04-09 13:39:46 +0300

    minor fix (temporary) reading credentials from local

--- src/main/java/util/DriveQuickstart.java
+++ src/main/java/util/DriveQuickstart.java
@@ -12,22 +12,20 @@ import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.DriveScopes;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+
+import java.io.*;
 import java.util.Collections;
 import java.util.List;
 
-// 123
 //todo: remove static logic entirely. singleton?
 public class DriveQuickstart {
-    private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
+    private static final java.io.File CREDENTIALS_FOLDER
+            = new java.io.File(System.getProperty("user.home"), "credentials");
+    private static final String CREDENTIALS_FILE_PATH = (CREDENTIALS_FOLDER.getPath() + "/credentials.json").replace("\\", "/");
+
     private static final String APPLICATION_NAME = "Google Drive API Java Quickstart";
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
 
-    private static final java.io.File CREDENTIALS_FOLDER
-            = new java.io.File(System.getProperty("user.home"), "credentials");
     private static FileDataStoreFactory DATA_STORE_FACTORY;
 
     private static final List<String> SCOPES = Collections.singletonList(DriveScopes.DRIVE);
@@ -41,8 +39,8 @@ public class DriveQuickstart {
             HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
             DATA_STORE_FACTORY = new FileDataStoreFactory(CREDENTIALS_FOLDER);
             driveService = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials())
-                .setApplicationName(APPLICATION_NAME)
-                .build();
+                    .setApplicationName(APPLICATION_NAME)
+                    .build();
             System.out.println("Google API сработало, авторизация прошла успешно");
         } catch (Throwable t) {
             t.printStackTrace();
@@ -51,10 +49,11 @@ public class DriveQuickstart {
     }
 
     private static Credential getCredentials() throws IOException {
-        InputStream in = DriveQuickstart.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
-        if (in == null) {
-            throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
-        }
+//        InputStream in = DriveQuickstart.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        InputStream in = new FileInputStream(CREDENTIALS_FILE_PATH);
+//        if (in == null) {
+//            throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
+//        }
         GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
         GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
                 HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
